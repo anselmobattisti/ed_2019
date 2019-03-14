ab022e8cf29a22820e3bd125f251c230429fcf45
Anselmo Battisti <anselmobattisti@gmail.com>
2019-03-14 20:49:21 -0300

    Commit Inicial
    Aula 1 - 14/03/2019

---
+++ README.md
@@ -0,0 +1,6 @@
+# Estrutura de Dados
+
+Esse repositório contém os exemplos e exercícios feitos na disciplina de Estrutura de Dados do primeiro semestre de 2019.
+
+Professora Isabel Rosseti.
+http://www.ic.uff.br/~rosseti/
---
+++ aula1/exemplo_1.c
@@ -0,0 +1,23 @@
+// includes das libbs
+#include <stdio.h>
+#include <stdlib.h>
+
+// cabeçalhos das funções
+float c2f(float);
+
+// método main
+int main (void) {
+  float c = 0;
+
+  printf("Digite a temperatura em graus Célcius: ");
+  scanf("%f",&c);
+
+  printf("A temperatura convertida em Fahrenheit é de: %f \n\n",c2f(c));
+
+  return 0;
+}
+
+// início das funções
+float c2f(float c) {
+  return ((c*9)/5)+32;
+}
