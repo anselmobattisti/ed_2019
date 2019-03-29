9819b205694fcf19a3bafae9dcdc841382a689df
Anselmo Battisti <anselmobattisti@gmail.com>
2019-03-29 19:38:05 -0300

    organização das pastas
    organização das pastas

---
+++ aula2/flavio/A03IntroducaoVetor.c
@@ -0,0 +1,20 @@
+//Vetores est�ticos
+#include <stdio.h>
+void main(){
+    int n;
+    printf("Digite o tamanho do vetor, após isso, os valores:\n");
+    scanf(" %d", &n);
+    if(n<=0)
+        return 0;
+    int vet[n];
+    for(int i=0; i<n; i++)
+        scanf(" %d", &vet[i]);
+    dobra(n,vet);
+    for(int i=0; i<n; i++)
+        printf("%d ", vet[i]);
+}
+void dobra(int n, int *vet){
+    for(int i=0; i<n; i++)
+        vet[i] *= 2;
+}
+
