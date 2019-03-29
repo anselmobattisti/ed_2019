04dabda5221390b24d713e81be2d705feda55456
Anselmo Battisti <anselmobattisti@gmail.com>
2019-03-29 18:36:22 -0300

    Update q1.c

--- lista3/anselmo/q1.c
+++ lista3/anselmo/q1.c
@@ -40,21 +40,25 @@ void shift_troca_string (char *str) {
   if (str[0]) {
     int c = (int) str[0];
 
+    // se for um A vira z
     if (c == 65) {
       printf("a");
       //str[0] = 122;
     }
 
+    // se for um a vira Z
     if (c == 97) {
       printf("Z");
       //str[0] = 90;
     }
 
+    // se estiver entre B e Z soma 31
     if (c >= 66 && c <= 90) {
       printf("%c",str[0]+31);
       //str[0] += 31;
     }
 
+    // se estiver entre b e z subtrai 33
     if (c >= 98 && c <= 122) {
       printf("%c",str[0]-33);
       //str[0] -= 33;
