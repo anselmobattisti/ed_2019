ebf084eba5eaedfa2671d0314a9f434344bc196d
Anselmo Battisti <anselmobattisti@gmail.com>
2019-03-30 19:27:12 -0300

    Create q5.c

---
+++ lista1/anselmo/q5.c
@@ -0,0 +1,30 @@
+/*
+Q5) Implemente um programa que, infinitamente, receba, como parâmetro de entrada, um número
+n e retorne a representação binária de n. Por exemplo, se n é igual a 12, a resposta deste
+programa deve ser “1100”. Seu programa para quando n for menor que zero
+*/
+#include <stdio.h>
+
+char bin[8];
+
+int main(void) {
+  int n, r;
+  int i = 0;
+  printf("\nDigite um número menor do que 255: ");
+  scanf("%d",&n);
+
+  while (n > 0) {
+    bin[i] = n%2;
+    n = n/2;
+    i++;
+  }
+
+
+  printf("\nEm binário o valor é: ");
+  for(int i = 7; i >= 0; i--) {
+    printf("%d",bin[i]);
+  }
+  printf("\n");
+
+  return 0;
+}
