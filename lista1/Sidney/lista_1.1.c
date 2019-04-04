81e6857a3656eb4d7addcb8f0a35b183edd79ebf
loyoladesa <49080717+loyoladesa@users.noreply.github.com>
2019-04-04 11:53:26 -0300

    Update lista_1.1.c

--- lista1/Sidney/lista_1.1.c
+++ lista1/Sidney/lista_1.1.c
@@ -1,5 +1,15 @@
-// Escrito por Sidney Loyola de Sá
-// Data: 30/03/2019
+/*
+Escrito por Sidney Loyola de Sá
+ Data: 30/03/2019
+ 
+ Q1) Escreva um programa que, infinitamente, leia um número inteiro n, que representa o tamanho,
+seguida de uma sequência de n números inteiros, e determine o comprimento máximo de um
+segmento crescente destes n números. Exemplos:
+ Na sequência 5, 10, 3, 2, 4, 7, 9, 8, 5 o comprimento do segmento crescente máximo é 4.
+ Na sequência 10, 8, 7, 5, 2 o comprimento de um segmento crescente máximo é 1.
+Seu programa para quando n for menor ou igual a zero.
+
+ */
 
 #include <stdio.h>
 
