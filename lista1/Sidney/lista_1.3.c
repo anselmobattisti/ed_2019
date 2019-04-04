99614320ae2a9c231293e0e91718f7cc86c93d0c
loyoladesa <49080717+loyoladesa@users.noreply.github.com>
2019-04-04 12:18:45 -0300

    Update lista_1.3.c

--- lista1/Sidney/lista_1.3.c
+++ lista1/Sidney/lista_1.3.c
@@ -1,6 +1,13 @@
-// Escrito por Sidney Loyola de Sá
-// Data: 30/03/2019
-// Duvidas quanto ao m negativo
+/*Escrito por Sidney Loyola de Sá
+ Data: 30/03/2019
+ Duvidas quanto ao m negativo
+
+ Q3) Implemente um programa que, infinitamente, receba, como parâmetro de entrada, um número
+n e um número m, e retorne todas as potências de m, menores que n. Por exemplo, se n = 1000 e
+m = 2, seu programa deve imprimir os seguintes números: 1, 2, 4, 8, 16, 32, 64, 128, 256 e 512.
+Seu programa para quando n for menor que 1 ou quando m for 0, ou 1 ou -1.
+ 
+ */
 #include <stdio.h>
 #include <math.h> //lembrar de incluir o -lm no final na hora de compilar
 
@@ -54,7 +61,6 @@ int encontrar(int n, int m){
 	}	
 	return tamanho;
 }
-
 
 
 
