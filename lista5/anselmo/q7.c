6766b1082fff059b33b9a55275490b7c987a9b59
Anselmo Battisti <anselmobattisti@gmail.com>
2019-04-13 16:35:46 -0300

    Create q7.c
    não é otimizado mas é uma solução

---
+++ lista5/anselmo/q7.c
@@ -0,0 +1,108 @@
+/*
+Q7) Refaça Q6, alterando a lista passada como parâmetro. O protótipo desta função é o
+seguinte: void* i_p ( TLSE *l).
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct lista {
+  int info;
+  struct lista *prox;
+}TLSE;
+
+TLSE* inicializa();
+TLSE* ins_fim(TLSE *l, int x);
+void i_p(TLSE* l);
+void imprime(TLSE*l);
+
+int main(void) {
+  TLSE *l = inicializa();
+
+  l = ins_fim(l, 1);
+  l = ins_fim(l, 2);
+  l = ins_fim(l, 3);
+  l = ins_fim(l, 4);
+  l = ins_fim(l, 5);
+
+  printf("\nLista Original:\n");
+  imprime(l);
+
+  printf("\nPrimeiro impar depois par:\n");
+  i_p(l);
+  imprime(l);
+}
+
+void i_p(TLSE* l) {
+
+  TLSE *lp, *li, *p = l;
+
+  lp = inicializa();
+  li = inicializa();
+
+  while (p) {
+    if (p->info % 2 == 1) {
+      li = ins_fim(li, p->info);
+    } else {
+      lp = ins_fim(lp, p->info);
+    }
+    p = p->prox;
+  }
+
+  // alterar todos os elementos da lista original
+  // com base nas cópias
+  p = li;
+  TLSE *aux = l;
+  while (p) {
+    aux->info = p->info;
+    p = p->prox;
+    if (p->prox) {
+      aux = aux->prox;
+    }
+  }
+
+  p = lp;
+  TLSE *aux = l;
+  while (p) {
+    aux->info = p->info;
+    p = p->prox;
+    aux = aux->prox;
+  }
+}
+
+void imprime(TLSE*l) {
+  printf("\nLista\n");
+  if (l) {
+    TLSE *p = l;
+    while (p->prox) {
+      printf("%d - ", p->info);
+      p = p->prox;
+    }
+    // imprime o últiom elemento da lista
+    printf("%d - ", p->info);
+  }
+  printf("\n\n");
+}
+
+
+TLSE* ins_fim(TLSE *l, int x) {
+  TLSE *novo = (TLSE*) malloc(sizeof(TLSE));
+  novo->info = x;
+  novo->prox = NULL;
+
+  if (!l) {
+    return novo;
+  }
+
+  TLSE *p = l;
+  while (p->prox) {
+    p = p->prox;
+  }
+
+  p->prox = novo;
+
+  return l;
+}
+
+TLSE* inicializa()  {
+  return NULL;
+}
