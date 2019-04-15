bc202e98d42153ae8ea498ba8b395ee3ada800e9
Anselmo Battisti <anselmobattisti@gmail.com>
2019-04-15 11:39:27 -0300

    Update raissa.c

--- coisas_aleatorias/anselmo/raissa.c
+++ coisas_aleatorias/anselmo/raissa.c
@@ -14,15 +14,16 @@ int main(void) {
     mat[i] = i;
   }
 
+  printf("\nMatriz Original\n");
   for(int i = 0; i<n_linhas; i++) {
     printf("%d - ",mat[i]);
     if (i%n == n-1)
       printf("\n");
   }
 
-  printf("\n\nRemovida\n\n");
-
-  aux = complemento(n,1,1,mat);
+  int k = 2, w = 1;
+  printf("\nMatriz Complementar removendo linha: %d e coluna: %d.\n",k,w);
+  aux = complemento(n,k,w,mat);
   for(int i = 0; i<((n-1)*(n-1)); i++) {
     printf("%d - ",aux[i]);
     if (i%n == n-2)
@@ -35,28 +36,32 @@ int main(void) {
 
 int * complemento(int n, int i, int j, int *mat) {
 
-  int n_linhas = (n-1)*(n-1);
+  int n_linhas = (n-1)*(n-1); // número de elementos do vetor resultante
   int *aux_vet = (int*) malloc(sizeof(int)*n_linhas);
 
-  int inicio_linha = n*i; // linha
-  int fim_linha = inicio_linha + n; // coluna
+  int inicio_linha = n*i; // início da linha na tabela linha
+  int fim_linha = inicio_linha + n; // fim da linha na tabela linha
+
+  // índica no vetor resultante
   int c = 0;
+
+  int rem_coluna = j; // primeria coluna que será removida
   for (int i = 0; i < n*n; i++) {
+
+    // pula a coluna
+    if (i == rem_coluna) {
+      rem_coluna += n;
+      continue;
+    }
+
     // pula a linha
     if (i>=inicio_linha && i < fim_linha) {
       continue;
     }
 
-    // pula a coluna
-    if (i%n == 0) {
-      continue;
-    }
-
-    printf("****%d****%d\n",i,i%n);
     aux_vet[c] = mat[i];
     c++;
   }
 
   return aux_vet;
-
 }
