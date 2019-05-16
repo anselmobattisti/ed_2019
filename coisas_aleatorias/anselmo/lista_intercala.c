83f4e05ad29ffc3ba5a71c9e4e4477a2460a2650
Anselmo Battisti <anselmobattisti@gmail.com>
2019-05-15 23:13:45 -0300

    Update lista_intercala.c

--- coisas_aleatorias/anselmo/lista_intercala.c
+++ coisas_aleatorias/anselmo/lista_intercala.c
@@ -8,26 +8,48 @@ typedef struct lista {
 
 TLSE* inicializa();
 TLSE* ins_ini(TLSE *l, int x);
+TLSE* ins_fim(TLSE *l, int x);
+
 void imprime(TLSE*l);
 void intercala(TLSE*l1, TLSE*l2);
+TLSE* intercala_2(TLSE*l1, TLSE*l2);
 
 int main(void) {
   TLSE *l1 = inicializa();
-  l1 = ins_ini(l1, 40);
-  l1 = ins_ini(l1, 30);
-  l1 = ins_ini(l1, 20);
-  l1 = ins_ini(l1, 10);
-
-  imprime(l1);
+  l1 = ins_fim(l1, 40);
+  l1 = ins_fim(l1, 30);
+  l1 = ins_fim(l1, 20);
+  l1 = ins_fim(l1, 10);
 
   TLSE *l2 = inicializa();
-  l2 = ins_ini(l2, 41);
+  l2 = ins_fim(l2, 41);
 
-  imprime(l2);
+  TLSE *l3 = intercala_2(l1,l2);
 
-  intercala(l1,l2);
+  imprime(l3);
+}
 
-  imprime(l1);
+TLSE* intercala_2(TLSE*l1, TLSE*l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
+
+  TLSE *l3 = inicializa();
+  TLSE *a1 = l1, *a2 = l2;
+  while (a1) {
+    l3 = ins_fim(l3,a1->info);
+    a1 = a1->prox;
+    if (a2) {
+      l3 = ins_fim(l3,a2->info);
+      a2 = a2->prox;
+    }
+  }
+
+  while(a2) {
+    l3 = ins_fim(l3, a2->info);
+    a2 = a2->prox;
+  }
+
+  return l3;
 }
 
 void intercala(TLSE*l1, TLSE*l2) {
@@ -37,7 +59,6 @@ void intercala(TLSE*l1, TLSE*l2) {
   TLSE *a1 = l1, *a2 = l2, *aux;
 
   while (a2) {
-
     if (a1->prox) {
       aux = a2;
       a2 = a2->prox;
@@ -72,6 +93,21 @@ TLSE* ins_ini(TLSE *l, int x) {
   return novo;
 }
 
+TLSE* ins_fim(TLSE *l, int x) {
+  if (!l) {
+    return ins_ini(l,x);
+  } else {
+    TLSE *p = l;
+    while(p->prox) {
+      p = p->prox;
+    }
+    p->prox = ins_ini(p->prox,x);
+    return l;
+  }
+}
+
+
 TLSE* inicializa()  {
   return NULL;
+  //return (TLSE*) malloc(sizeof(TLSE));
 }
